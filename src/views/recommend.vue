cdf5698a199b510eeb1f71846c66ce2495c68e5b
changlin93 <767425412@qq.com>
2021-04-15 07:53:50 +0800

    :sparkles: 完善轮播组件开发

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,12 +1,56 @@
 <template>
-    <div class="recommend">recommend</div>
+    <div class="recommend">
+        <div class="slider-wrapper">
+            <div class="slider-content">
+                <slider v-if="sliders.length" :sliders="sliders"></slider>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { getRecommend } from '@/service/recommend';
+import Slider from '@/components/base/slider/slider';
 export default {
     name: 'recommend',
-    components: {},
+    components: {
+        Slider,
+    },
+    data() {
+        return {
+            sliders: [], // 轮播图
+            albums: [],
+        };
+    },
+    async created() {
+        const result = await getRecommend();
+        this.sliders = result.sliders;
+        this.albums = result.albums;
+    },
+    methods: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.recommend {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    .slider-wrapper {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-top: 40%;
+        overflow: hidden;
+        .slider-content {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+</style>
