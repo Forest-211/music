6cdddf908995d2e824bb4f404fc57c82616d13a3
changlin93 <767425412@qq.com>
2021-04-15 22:32:36 +0800

    :sparkles: 完善歌单列表和滚动组件的封装

---
+++ src/components/base/scroll/scroll.vue
@@ -0,0 +1,29 @@
+<template>
+    <div ref="rootRef">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import UseScroll from './use-scroll';
+export default {
+    name: 'scroll',
+    props: {
+        click: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    setup(props) {
+        const rootRef = ref(null);
+        UseScroll(rootRef, props);
+
+        return {
+            rootRef,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,10 +1,31 @@
 <template>
     <div class="recommend">
-        <div class="slider-wrapper">
-            <div class="slider-content">
-                <slider v-if="sliders.length" :sliders="sliders"></slider>
+        <scroll class="recommend-content">
+            <div>
+                <div class="slider-wrapper">
+                    <div class="slider-content">
+                        <slider
+                            v-if="sliders.length"
+                            :sliders="sliders"
+                        ></slider>
+                    </div>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li class="item" v-for="item in albums" :key="item.id">
+                            <div class="icon">
+                                <img :src="item.pic" width="60" height="60" />
+                            </div>
+                            <div class="text">
+                                <h2 class="name">{{ item.username }}</h2>
+                                <p class="title">{{ item.title }}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </scroll>
     </div>
 </template>
 
@@ -12,10 +33,13 @@
 // @ is an alias to /src
 import { getRecommend } from '@/service/recommend';
 import Slider from '@/components/base/slider/slider';
+import Scroll from '../components/base/scroll/scroll';
+
 export default {
     name: 'recommend',
     components: {
         Slider,
+        Scroll,
     },
     data() {
         return {
@@ -38,18 +62,60 @@ export default {
     width: 100%;
     top: 88px;
     bottom: 0;
-    .slider-wrapper {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding-top: 40%;
+    overflow: scroll;
+    .recommend-content {
+        height: 100%;
         overflow: hidden;
-        .slider-content {
-            position: absolute;
-            left: 0;
-            top: 0;
+        .slider-wrapper {
+            position: relative;
             width: 100%;
-            height: 100%;
+            height: 0;
+            padding-top: 40%;
+            overflow: hidden;
+            .slider-content {
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+            }
+        }
+        .recommend-list {
+            .list-title {
+                height: 65px;
+                line-height: 65px;
+                text-align: center;
+                font-size: $font-size-medium;
+                color: $color-theme;
+            }
+            .item {
+                display: flex;
+                box-sizing: border-box;
+                align-items: center;
+                padding: 0 20px 20px 20px;
+
+                .icon {
+                    flex: 0 0 60px;
+                    width: 60px;
+                    padding-right: 20px;
+                }
+                .text {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    flex: 1;
+                    line-height: 20px;
+                    overflow: hidden;
+                    font-size: $font-size-medium;
+                }
+                .name {
+                    margin-bottom: 10px;
+                    color: $color-text;
+                }
+                .title {
+                    color: $color-text-d;
+                }
+            }
         }
     }
 }
